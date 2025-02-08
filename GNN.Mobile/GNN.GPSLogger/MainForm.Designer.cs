51aa5859d48c4fa5012cceb53b521d21a4b9354c
Nanich87 <2477098+Nanich87@users.noreply.github.com>
2025-02-08 12:30:00 +0200

    Update

--- GNN.Mobile/GNN.GPSLogger/MainForm.Designer.cs
+++ GNN.Mobile/GNN.GPSLogger/MainForm.Designer.cs
@@ -30,14 +30,56 @@
         private void InitializeComponent()
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
-            this.Menu = this.mainMenu1;
-            this.components = new System.ComponentModel.Container();
+            this.menuItemStart = new System.Windows.Forms.MenuItem();
+            this.menuItemStop = new System.Windows.Forms.MenuItem();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItemStart);
+            this.mainMenu1.MenuItems.Add(this.menuItemStop);
+            // 
+            // menuItemStart
+            // 
+            this.menuItemStart.Text = "Start";
+            this.menuItemStart.Click += new System.EventHandler(this.menuItemStart_Click);
+            // 
+            // menuItemStop
+            // 
+            this.menuItemStop.Text = "Stop";
+            this.menuItemStop.Click += new System.EventHandler(this.menuItemStop_Click);
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelSpeed.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Regular);
+            this.labelSpeed.Location = new System.Drawing.Point(3, 0);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(234, 100);
+            this.labelSpeed.Text = "000.0";
+            this.labelSpeed.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.Text = "Form1";
             this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.labelSpeed);
+            this.Menu = this.mainMenu1;
+            this.Name = "MainForm";
+            this.Text = "GPS Logger";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuItem menuItemStart;
+        private System.Windows.Forms.MenuItem menuItemStop;
+        private System.Windows.Forms.Label labelSpeed;
     }
 }
 
--- GNN.Mobile/GNN.GPSLogger/MainForm.cs
+++ GNN.Mobile/GNN.GPSLogger/MainForm.cs
@@ -1,19 +1,106 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
-
-namespace GNN.GPSLogger
+﻿namespace GNN.GPSLogger
 {
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.IO;
+    using System.IO.Ports;
+    using System.Text;
+    using System.Windows.Forms;
+
     public partial class MainForm : Form
     {
+        private SerialPort serialPort;
+
         public MainForm()
         {
             InitializeComponent();
+
+            serialPort = new SerialPort();
+            serialPort.PortName = "COM1";
+            serialPort.BaudRate = 9600;
+            serialPort.Parity = Parity.None;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+
+            menuItemStart.Enabled = true;
+            menuItemStop.Enabled = false;
+        }
+
+        private void menuItemStart_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                return;
+            }
+
+            var success = false;
+
+            try
+            {
+                serialPort.Open();
+                success = true;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation,
+                    MessageBoxDefaultButton.Button1);
+            }
+
+            if (success)
+            {
+                menuItemStart.Enabled = false;
+                menuItemStop.Enabled = true;
+            }
+        }
+
+        private void menuItemStop_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                return;
+            }
+
+            var success = false;
+
+            try
+            {
+                serialPort.Close();
+                success = true;
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(
+                   ex.Message,
+                   "Error",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Exclamation,
+                   MessageBoxDefaultButton.Button1);
+            }
+
+            if (success)
+            {
+                menuItemStop.Enabled = false;
+                menuItemStart.Enabled = true;
+            }
+        }
+
+        void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var speed = 0.5;
+
+            labelSpeed.BeginInvoke((Action)(() =>
+            {
+                labelSpeed.Text = string.Format("{0:000.0}", speed);
+            }));
         }
     }
 }
